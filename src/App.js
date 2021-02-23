d770ce40761d69f416c07fc8bb9b35243d98910f
Naman Dutta <37544286+duttanaman1@users.noreply.github.com>
2021-02-24 02:09:22 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Home from "./app/components/Home";
 import Profile from "./app/components/Profile";
 import cart from "./app/components/cart";
 import Orders from "./app/components/Orders";
+import Product from "./app/components/Product";
+
+import Career from "./app/components/career";
 
 import SignUp from "./app/auth/SignUp";
 import Signin from "./app/auth/Signin";
 import SignOut from "./app/auth/SignOut";
 import forgotpass from "./app/auth/forgotpass";
 import termCondition from "./app/auth/termCondition";
+import AuthenticationService from "./app/services/AuthenticationService";
 
 class App extends Component {
   render() {
@@ -25,10 +29,20 @@ class App extends Component {
           <Route path="/forgotpass" component={forgotpass} />
           <Route path="/termCondition" component={termCondition} />
 
+          <Route path="/career" component={Career} />
+
           <Route path="/home" exact={true} component={Home} />
-          <Route path="/cart" exact={true} component={cart} />
-          <Route path="/profile" exact={true} component={Profile} />
-          <Route path="/orders" exact={true} component={Orders} />
+          {AuthenticationService.getCurrentUser() && (
+            <Route path="/cart" exact={true} component={cart} />
+          )}
+          {AuthenticationService.getCurrentUser() && (
+            <Route path="/profile" exact={true} component={Profile} />
+          )}
+
+          {AuthenticationService.getCurrentUser() && (
+            <Route path="/orders" exact={true} component={Orders} />
+          )}
+          <Route path="/Product/:product_id" component={Product} />
         </Switch>
       </Router>
     );
