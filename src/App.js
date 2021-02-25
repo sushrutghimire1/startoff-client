d519d27d54a84eb1d7ca29ba37cc41537eb09062
Naman Dutta <37544286+duttanaman1@users.noreply.github.com>
2021-02-25 15:08:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import Profile from "./app/components/Profile";
 import cart from "./app/components/cart";
 import Orders from "./app/components/Orders";
 import Product from "./app/components/Product";
+import Search from "./app/components/search";
 
 import Career from "./app/components/career";
 
 import SignUp from "./app/auth/SignUp";
+import FillDetails from "./app/auth/fillDetails";
 import Signin from "./app/auth/Signin";
 import SignOut from "./app/auth/SignOut";
 import forgotpass from "./app/auth/forgotpass";
@@ -24,6 +26,7 @@ class App extends Component {
         <Switch>
           <Route path="/" exact={true} component={Signin} />
           <Route path="/signin" exact={true} component={Signin} />
+          <Route path="/fillDetails" exact={true} component={FillDetails} />
           <Route path="/signup" exact={true} component={SignUp} />
           <Route path="/signout" component={SignOut} />
           <Route path="/forgotpass" component={forgotpass} />
@@ -42,7 +45,9 @@ class App extends Component {
           {AuthenticationService.getCurrentUser() && (
             <Route path="/orders" exact={true} component={Orders} />
           )}
-          <Route path="/Product/:product_id" component={Product} />
+          <Route path="/product/:product_id" component={Product} />
+
+          <Route path="/search/:value" component={Search} />
         </Switch>
       </Router>
     );
